6bf86684429b06936fbb3c64cbea68e7d85d2408
snail215 <ekdms0736@naver.com>
2023-11-07 04:02:20 +0900

    학습 내용 보완

--- JavaScript/1102/d_operator/script.js
+++ JavaScript/1102/d_operator/script.js
@@ -1,5 +1,6 @@
 //! JavaScript (ES6) code snippet
-// 
+// 코드 스니펫
+// : 코드를 더 빠르게 작성할 수 있도록 도와주는 작은 코드 조각
 
 // 연산자(Operator)
 // 산술 / 할당 / 비교 / 논리
@@ -18,16 +19,16 @@
 let x = 10;
 let y = 3;
 
-console.log(x + y);
-console.log(x - y);
-console.log(x * y);
-console.log(x / y);
-console.log(x % y);
+console.log(x + y); // 13
+console.log(x - y); // 7
+console.log(x * y); // 30
+console.log(x / y); // 3.3333333...
+console.log(x % y); // 1
 
-console.log(x++);
-console.log(x);
+console.log(x++); // 10
+console.log(x); // 11
 
-console.log(--x);
+console.log(--x); // 10
 
 // 증감 연산자
 // : 숫자에 직접 적용 X
@@ -55,14 +56,17 @@ console.log(3 - 1);
 
 let a = 10; // 10을 a에 할당
 
-a += 5; // a = a + 5;
-console.log(a);
+a += 5; // a = a + 5; >> 15 = 10 + 5;
+console.log(a); // 15
 
 a -= 2; // a = a - 2;
 console.log(a); // 13
 
 a *= 2; // a = a * 2;
 console.log(a); // 26
+
+a /= 2; // a = a / 2;
+console.log(a); // 13
 
 //! 비교 연산자
 // : 두 값을 비교하고, 그 결과를 boolean의 값으로 반환
